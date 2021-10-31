5ad34f8b983607aee58d106c5b399e80f7bceb69
marcoskloss <marcoskloss2001@gmail.com>
2021-10-31 19:38:14 -0300

    add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import { BrowserRouter } from 'react-router-dom';
 import { Routes } from './Routes';
 import { GlobalStyle } from './styles/global';
+import { AuthContextProvider } from './context/AuthContext';
 
 function App() {
     return (
         <BrowserRouter>
             <GlobalStyle />
-            <Routes />
+            <AuthContextProvider>
+                <Routes />
+            </AuthContextProvider>
         </BrowserRouter>
     );
 }
--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,14 +1,19 @@
 import { Switch, RouteProps, Redirect, Route } from 'react-router-dom';
+import { useAuth } from './context/AuthContext';
 import { Home } from './pages/Home';
-import { Login } from './public/pages/Login';
-import { SignUp } from './public/pages/SignUp';
+import { Login } from './pages/public/pages/Login';
+import { SignUp } from './pages/public/pages/SignUp';
 
 interface CustomRouteProps extends RouteProps {
     isPrivate?: boolean;
 }
 
 function CustomRoute({ isPrivate, ...rest }: CustomRouteProps) {
-    const isAuthenticated = false;
+    const { isAuthenticated, loading } = useAuth();
+
+    if (loading) {
+        return <h1>Loading...</h1>;
+    }
 
     if (isPrivate && !isAuthenticated) {
         return <Redirect to="/login" />;
@@ -21,13 +26,17 @@ function CustomRoute({ isPrivate, ...rest }: CustomRouteProps) {
     return <Route {...rest} />;
 }
 
+function NotFound() {
+    return <p>not found</p>;
+}
+
 export function Routes() {
     return (
         <Switch>
             <CustomRoute exact path="/login" component={Login} />
             <CustomRoute exact path="/sign-up" component={SignUp} />
             <CustomRoute isPrivate exact path="/home" component={Home} />
-            <CustomRoute exact path="/" component={SignUp} />
+            <Route path="/" component={NotFound} />
         </Switch>
     );
 }
